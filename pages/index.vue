c865d52ed5adc94019b2618e52cf3f525d82775f
Emanuel Machado <emachado@emachado.pt>
2019-11-01 20:59:39 +0000

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,40 @@
     justify-center
     align-center
   >
+    <v-col
+      v-for="(tool, index) in tools"
+      :key="index"
+    >
+      {{ tool }}
+    </v-col>
 
+    <v-btn
+      fixed
+      dark
+      fab
+      bottom
+      right
+      large
+      color="primary"
+      class="mb-7"
+      to="/new"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
   </v-layout>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    tools () {
+      return this.$store.state.localStorage.tools
+    }
+  },
+  head () {
+    return {
+      title: 'Tools'
+    }
+  }
+}
 </script>
---
+++ store/localStorage.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  tools: []
+})
+
+export const mutations = {
+
+}
